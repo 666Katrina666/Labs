3a6477a1683fe96e744b117e854b307a2fbf7e46
666Katrina666 <90830504+666Katrina666@users.noreply.github.com>
2021-09-20 21:10:45 +0300

    Add files via upload

---
+++ lab 4.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << "№1" << endl;
+    float a;
+    float b;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    float S = a * b;
+    float P = 2 * (a + b);
+    cout << "S = " << S << ", P = " << P << endl;
+
+    cout << endl << "№2" << endl;
+    float π = 3.14;
+    cout << "d =";
+    float d;
+    cin >> d;
+    float L = π * d;
+    cout << "L = " << L << endl;
+
+    cout << endl << "№3" << endl;
+    cout << "a = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+    cout << "average = " << (a + b) / 2 << endl;
+
+    cout << endl << "№4" << endl << "give me first number: "; 
+    cin >> a;
+    cout << "give me second number: ";
+    cin >> b;
+    cout << "sum = " << (sqrt(a) + sqrt(b)) << ", difference = " << abs(sqrt(a) - sqrt(b)) << ", multiplication = " << (pow(a, 2) * pow(b, 2)) << ", private = " << (pow(a, 2) / pow(b, 2)) << endl;
+
+    cout << endl << "№4" << endl << "give me first number: ";
+    cin >> a;
+    cout << "give me second number: ";
+    cin >> b;
+    a = abs(a);
+    if (b < 0) {
+        b *= -1;
+    }
+    cout << "sum = " << (a + b) << ", difference = " << abs(a - b) << ", multiplication = " << (a * b) << ", private = " << (a / b) << endl;
+
+    return 0;
+}
